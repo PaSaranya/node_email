d95f11b5ac2e170426da824bb1cdd1b8d4b5a249
reddyshanmukhasandeep <reddyshanmukha.sandeep@cognizant.com>
2016-12-15 15:42:22 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -4,7 +4,7 @@ y=3
 if [ $x -eq $y ];
 then
 echo "Running a sample shellscript";
-echo "java -jar sample-0.0.1-SNAPSHOT.jar"
+echo "java -jar samplefail-0.0.1-SNAPSHOT.jar"
 else
 echo " failed" ; exit 1; 
 fi
