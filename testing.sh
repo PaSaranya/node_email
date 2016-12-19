d6c67c3c354c6e4027946dfe58d1d228629004cb
reddyshanmukhasandeep <reddyshanmukha.sandeep@cognizant.com>
2016-12-19 12:38:06 +0530

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -3,7 +3,7 @@ x=3
 y=3
 if [ $x -eq $y ];
 then
-echo "Running a sample shellscript";
+echo "Running a sample shellscript1";
 java -jar resource-web-app/samplefail-0.0.1-SNAPSHOT.jar
 else
 echo " failed" ; exit 1; 
